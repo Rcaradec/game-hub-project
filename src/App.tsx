48f097f33a509745ecfe9bac76d98bb349fd6a80
Remi Caradec <72319136+Rcaradec@users.noreply.github.com>
2024-10-02 23:19:32 +0200

    Add Responsive Layout (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import { useState } from "react";
 import "./App.css";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Button colorScheme="blue">Button</Button>
+      <Grid
+        gridTemplateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="dodgerBlue">
+          Main
+        </GridItem>
+      </Grid>
     </>
   );
 }
