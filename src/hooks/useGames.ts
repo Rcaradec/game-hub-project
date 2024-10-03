b1b867cedb23cfe8e2568c8b2031d81f78319203
Remi Caradec <72319136+Rcaradec@users.noreply.github.com>
2024-10-04 00:06:13 +0200

    Add platforms Icons (#8)

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -7,10 +7,17 @@ export type gamesResponse = {
   results: Game[];
 };
 
+export type Platform = {
+  id: number;
+  name: string;
+  slug: string;
+};
+
 export type Game = {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 };
 
 const useGames = () => {
